75ab827e657afa861e1de8ac78b82f4b415fe2d7
Ashith V L <ashithvl@gmail.com>
2022-07-26 08:56:49 +0530

    7. Qualifier

--- src/main/java/com/codeWithAshith/SpringBasics/BinarySearchImpl.java
+++ src/main/java/com/codeWithAshith/SpringBasics/BinarySearchImpl.java
@@ -1,12 +1,14 @@
 package com.codeWithAshith.SpringBasics;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
 
     @Autowired
+    @Qualifier("bubble")
     private SortAlgorithm bubbleSortAlgorithm;
 
     public int binarySearch(int[] numbers, int numberToSearchFor) {
