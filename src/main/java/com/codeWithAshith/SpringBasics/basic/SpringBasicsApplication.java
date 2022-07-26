c2f7e6acaaafcc949c853f1084d1d4c2044116b9
Ashith V L <ashithvl@gmail.com>
2022-07-26 11:30:12 +0530

    13. Without spring boot

--- src/main/java/com/codeWithAshith/SpringBasics/basic/SpringBasicsApplication.java
+++ src/main/java/com/codeWithAshith/SpringBasics/basic/SpringBasicsApplication.java
@@ -12,12 +12,14 @@ public class SpringBasicsApplication {
     public static void main(String[] args) {
 
         // Application Context
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringBasicsApplication.class);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringBasicsApplication.class);
         BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
 
         System.out.println(binarySearch);
 
         int result = binarySearch.binarySearch(new int[]{1, 5, 3, 6, 10}, 3);
+
+        applicationContext.close();
 
         System.out.println(result);
     }
